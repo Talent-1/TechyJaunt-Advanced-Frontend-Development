b1927ba0c51080ef40f9616594dadac59c827345
Talent-1 <itzmhiztatalent@gmail.com>
2025-01-08 23:12:00 +0100

    updated

---
+++ week 1/app.js
@@ -0,0 +1,11 @@
+//get the input elements
+const userName = document.getElementById('username').value;
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+//handles validation
+let valid = true;
+
+signUpForm.addEventListener('submit', (e) => {
+    Item.textContent = ' ';
+})
