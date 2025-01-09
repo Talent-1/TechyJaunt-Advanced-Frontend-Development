a859f8cbbd7ea2d7ca222ad1613da0d666b01e18
Talent-1 <itzmhiztatalent@gmail.com>
2025-01-09 08:02:54 +0100

    updated

--- week 1/app.js
+++ week 1/app.js
@@ -1,4 +1,6 @@
-//get the input elements
+
+signupForm.addEventListener('submit', (e) => {
+   //get the input elements
 const userName = document.getElementById('username').value;
 const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
@@ -6,6 +8,32 @@ const password = document.getElementById('password').value;
 //handles validation
 let valid = true;
 
-signUpForm.addEventListener('submit', (e) => {
-    Item.textContent = ' ';
+//clear previous error message
+[userNameError, emailError, passwordError].forEach(item => {
+    item.textContent =" ";
+})
+
+//validate username
+//username must be up to three characters
+if (username < 3) {
+    valid = false;
+    userName.textContent = 'username must have at least three charcters';
+    userNameError.style.display = 'block';
+}
+
+// validate email
+const emailPattern = /^(\w[2,50])@{a-z}[2, 20]\.{a-z}[2,8] /gi;
+// example_.123@email.com
+if (email != emailPattern) {
+    valid = false;
+    emailError.textContent = 'Please enter a valid email';
+    emailError.style.display = 'block';
+}
+
+//validate password
+if (password.length < 6) {
+    valid = false;
+    emailError.textContent = 'Password must be upto 6 character long and must contain one Uppercase, a number and a special character';
+    emailError.style.display = 'block';
+}
 })
